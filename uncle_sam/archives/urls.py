549bdfb22f56228187278746d762d29fc02ddd6a
Alex Rogers <alex.rogers823@gmail.com>
2025-03-04 15:05:40 -0800

    arranged archive data for chart display

--- uncle_sam/archives/urls.py
+++ uncle_sam/archives/urls.py
@@ -2,5 +2,6 @@ from archives import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.ArchiveList.as_view())
+    path('', views.ArchiveList.as_view()),
+    path('charts/', views.ArchiveChartList.as_view())
 ]
--- uncle_sam/archives/views.py
+++ uncle_sam/archives/views.py
@@ -17,3 +17,31 @@ class ArchiveList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ArchiveChartList(APIView):
+    def arrange_chart_data(self, serializer_data):
+        chart_types = ["Debt", "Saving", "Item"]
+        def get_chart_data(chart_name):
+            return filter(lambda x: x["type"] == chart_name, serializer_data)
+        
+        arranged = map(lambda chart_name: {
+            "type": chart_name,
+            "data": map(lambda instance: {
+                "title": instance["title"],
+                "history": [
+                    {
+                        "amount": instance["amount"],
+                        "date": instance["updated_date"]
+                    }
+                ]
+            }, get_chart_data(chart_name))
+        }, chart_types)
+
+        return arranged
+
+    def get(self, request, format=None) -> Response:
+        archives = Archive.objects.all()
+        serializer = ArchiveSerializer(archives, many=True)
+        chart_data = self.arrange_chart_data(serializer.data)
+        return Response(chart_data)
